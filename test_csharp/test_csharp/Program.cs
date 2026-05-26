4b3cd3e8436db9abc470af9d2c91726dfcf1f625
Orellana93 <lorellana2706@gmail.com>
2026-05-26 01:46:02 -0500

    fix: valida nulo al ingresar directo de consola

--- test_csharp/test_csharp/Program.cs
+++ test_csharp/test_csharp/Program.cs
@@ -1,40 +1,36 @@
 ﻿//Console.WriteLine("Hello, World!");
 // C# — recuerda if(){ } else{ }
-internal class Program
+Console.WriteLine("Por favor, ingresar nota de 0 a 20:");
+
+string? nota_s = Console.ReadLine();
+
+if (nota_s == null || nota_s.Trim() == "" || !int.TryParse(nota_s, out _))
 {
-    private static void Main(string[] args)
+    Console.WriteLine("Entrada no válida. Por favor, ingrese una nota entre 0 y 20.");
+    return;
+}
+
+int nota = Convert.ToInt32(nota_s);
+
+if (nota >= 0 && nota <= 20)
+{
+    if (nota >= 11)
     {
-        Console.WriteLine("Por favor, ingresar nota de 0 a 20:");
-        String nota_s = Console.ReadLine();
-        if (nota_s == null || nota_s.Trim() == "" || !int.TryParse(nota_s, out _))
+        if (nota == 20)
         {
-            Console.WriteLine("Entrada no válida. Por favor, ingrese una nota entre 0 y 20.");
-            return;
-        }
-
-        int nota = Convert.ToInt32(nota_s);
-
-        if (nota >= 0 && nota <= 20)
-        {
-            if (nota >= 11)
-            {
-                if (nota == 20)
-                {
-                    Console.WriteLine("Excelente");
-                }
-                else
-                {
-                    Console.WriteLine("Aprobado");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Desaprobado");
-            }            
+            Console.WriteLine("Excelente");
         }
         else
         {
-            Console.WriteLine("Nota no válida. Por favor, ingrese una nota entre 0 y 20.");
+            Console.WriteLine("Aprobado");
         }
     }
+    else
+    {
+        Console.WriteLine("Desaprobado");
+    }
+}
+else
+{
+    Console.WriteLine("Nota no válida. Por favor, ingrese una nota entre 0 y 20.");
 }
